9448f22766a3d9aba749e3051966c2f63cabedff
nogahm <nogahm@post.bgu.ac.il>
2018-06-11 20:04:48 +0300

    fff

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
         @Override
         public void start(Stage primaryStage) throws Exception{
-            Parent root = FXMLLoader.load(getClass().getResource("AddCoGUI.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
             primaryStage.setTitle("Mivhanet System");
             primaryStage.setScene(new Scene(root, 500, 500));
             primaryStage.show();
